600ffcaff53b7d73478b92ca36513087d2f9431d
Dalia Jaber <61179209+dalia-jaber@users.noreply.github.com>
2025-06-18 12:53:27 +0300

    Create livekit-agent.py

---
+++ livekit-agent.py
@@ -0,0 +1,105 @@
+import logging
+
+from dotenv import load_dotenv
+_ = load_dotenv(override=True)
+
+logger = logging.getLogger("dlai-agent")
+logger.setLevel(logging.INFO)
+
+from livekit import agents
+from livekit.agents import Agent, AgentSession, JobContext, WorkerOptions, jupyter
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+from livekit.plugins import (
+    openai,
+    silero,
+)
+
+from livekit.agents.metrics import LLMMetrics, STTMetrics, TTSMetrics, EOUMetrics
+import asyncio
+
+
+class MetricsAgent(Agent):
+    def __init__(self) -> None:
+        stt = openai.STT(model="gpt-4o-mini-transcribe", language="en")
+        llm = openai.LLM(model="gpt-4o-mini")
+        # tts = openai.TTS()
+        tts = openai.TTS(model='kokoro', base_url='http://localhost:8880/v1',
+                         instructions='you should be able to read acronyms'),
+        vad = silero.VAD.load()
+        turn_detection = MultilingualModel()
+
+        super().__init__(
+            instructions="You are a helpful assistant communicating via voice",
+            stt=stt,
+            llm=llm,
+            tts=tts,
+            vad=vad,
+            turn_detection=turn_detection
+        )
+
+        def llm_metrics_wrapper(metrics: LLMMetrics):
+            asyncio.create_task(self.on_llm_metrics_collected(metrics))
+
+        llm.on("metrics_collected", llm_metrics_wrapper)
+
+        def stt_metrics_wrapper(metrics: STTMetrics):
+            asyncio.create_task(self.on_stt_metrics_collected(metrics))
+
+        stt.on("metrics_collected", stt_metrics_wrapper)
+
+        def eou_metrics_wrapper(metrics: EOUMetrics):
+            asyncio.create_task(self.on_eou_metrics_collected(metrics))
+
+        stt.on("eou_metrics_collected", eou_metrics_wrapper)
+
+        def tts_metrics_wrapper(metrics: TTSMetrics):
+            asyncio.create_task(self.on_tts_metrics_collected(metrics))
+
+        tts.on("metrics_collected", tts_metrics_wrapper)
+
+    async def on_llm_metrics_collected(self, metrics: LLMMetrics) -> None:
+        print("\n--- LLM Metrics ---")
+        print(f"Prompt Tokens: {metrics.prompt_tokens}")
+        print(f"Completion Tokens: {metrics.completion_tokens}")
+        print(f"Tokens per second: {metrics.tokens_per_second:.4f}")
+        print(f"TTFT: {metrics.ttft:.4f}s")
+        print("------------------\n")
+
+    async def on_stt_metrics_collected(self, metrics: STTMetrics) -> None:
+        print("\n--- STT Metrics ---")
+        print(f"Duration: {metrics.duration:.4f}s")
+        print(f"Audio Duration: {metrics.audio_duration:.4f}s")
+        print(f"Streamed: {'Yes' if metrics.streamed else 'No'}")
+        print("------------------\n")
+
+    async def on_eou_metrics_collected(self, metrics: EOUMetrics) -> None:
+        print("\n--- End of Utterance Metrics ---")
+        print(f"End of Utterance Delay: {metrics.end_of_utterance_delay:.4f}s")
+        print(f"Transcription Delay: {metrics.transcription_delay:.4f}s")
+        print("--------------------------------\n")
+
+    async def on_tts_metrics_collected(self, metrics: TTSMetrics) -> None:
+        print("\n--- TTS Metrics ---")
+        # "TTFB is a metric that measures the time between the request for a resource and when the first byte of a response begins to arrive."
+        print(f"TTFB: {metrics.ttfb:.4f}s")
+        print(f"Duration: {metrics.duration:.4f}s")
+        print(f"Audio Duration: {metrics.audio_duration:.4f}s")
+        print(f"Streamed: {'Yes' if metrics.streamed else 'No'}")
+        print("------------------\n")
+
+
+async def entrypoint(ctx: JobContext):
+    await ctx.connect()
+
+    session = AgentSession()
+
+    await session.start(
+        agent=MetricsAgent(),
+        room=ctx.room,
+    )
+
+
+if __name__ == "__main__":
+    # Run the agent worker app with the entrypoint function
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
